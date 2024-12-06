082b22a84a022bd6a0225199694a2a40d005fba4
Bisratolera <leultolera@gmail.com>
2024-12-06 12:46:43 +0300

    hello world

---
+++ start.dart
@@ -0,0 +1,19 @@
+
+// void main() {
+//   // this is a single line comment
+//   /*this is multiple line comment */
+//   print('hello world');
+// }
+
+
+// creating ant
+void main() {
+  var con1 = 5;
+  var con2 = 3;
+
+  print("first ant: $con1");
+  print("first ant: $con2");
+
+
+}
+
